5540ab812ce07530ff9d403d9848e6c727283f20
Premkub49 <keerati.se123@gmail.com>
2026-01-07 12:25:53 +0700

    fix: add datatype to data arguement in transaction repo

--- app/repositories/transaction.py
+++ app/repositories/transaction.py
@@ -1,8 +1,10 @@
 from sqlalchemy.orm import Session
 
+from app.models.base import Transaction
+
 
 class TransactionRepo:
-    def create_transaction(self, db: Session, data):
+    def create_transaction(self, db: Session, data: Transaction):
         db.add(data)
         db.commit()
         return data
